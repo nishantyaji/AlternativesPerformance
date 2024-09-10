67e78797383ac6868cc889a0e54642cc5a6804c3
nishantyaji <nishant.yaji@gmail.com>
2024-09-11 00:14:05 +0530

    A simple test for testing whether math.gcd or a crude one code now is better

---
+++ TestGCD.py
@@ -0,0 +1,34 @@
+import datetime
+import math
+
+
+class TestGCD:
+
+    def test(self):
+        for fn in [TestGCD.gcd, math.gcd]:
+            start = datetime.datetime.now()
+            print("start: ", start)
+            for i in range(2000, 3000):
+                for j in range(3000, 4000):
+                    fn(i, j)
+            end = datetime.datetime.now()
+            print("end: ", end)
+            print("taken: ", end - start)
+        # The jury is that: math.gcd is faster, way faster
+
+    def gcd(a: int, b: int):
+        small = b if a > b else a
+        large = a ^ b ^ small
+
+        r = 1
+        while r > 0:
+            r = large % small
+            large = small
+            small = r
+
+        return large
+
+
+if __name__ == '__main__':
+    t = TestGCD()
+    t.test()
