52504e5a7b7d44df04dfdf5e68514f86fccb5f65
nishantyaji <nishant.yaji@gmail.com>
2024-09-11 01:15:31 +0530

    GCD performance. Some tweaks.

--- TestGCD.py
+++ TestGCD.py
@@ -5,7 +5,7 @@ import math
 class TestGCD:
 
     def test(self):
-        for fn in [TestGCD.gcd, math.gcd]:
+        for fn in [TestGCD.gcd, TestGCD.math_gcd]:
             start = datetime.datetime.now()
             print("start: ", start)
             for i in range(2000, 3000):
@@ -14,7 +14,8 @@ class TestGCD:
             end = datetime.datetime.now()
             print("end: ", end)
             print("taken: ", end - start)
-        # The jury is that: math.gcd is faster, way faster
+            # The jury is that: math.gcd is faster, way faster
+
 
     def gcd(a: int, b: int):
         small = b if a > b else a
@@ -27,6 +28,10 @@ class TestGCD:
             small = r
 
         return large
+
+
+    def math_gcd(a: int, b: int):
+        return math.gcd(a, b)
 
 
 if __name__ == '__main__':
